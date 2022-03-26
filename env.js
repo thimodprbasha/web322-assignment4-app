a97b709ffabda40fcf1f7f089e81da3a45b852ef
thimodprbasha <thimodprabasha@icloud.com>
2022-03-27 02:38:52 +0530

    added db config

--- env.js
+++ env.js
@@ -1,5 +1,24 @@
-module.exports = Object.freeze({
-  CLOUD_NAME: "testappdinj123q",
-  API_KEY: "947624955641163",
-  API_SECRET: "9LNbFvteQzDqnx4JMhN1WE0209U",
-});
+module.exports = {
+  DB: {
+    USER: "fesjaqoahlltoh",
+    HOST: "ec2-52-3-60-53.compute-1.amazonaws.com",
+    DATABASE: "daugvo8icimo4b",
+    PASSWORD:
+      "ef6090a7ee2cc42a564ebcf496c19ca0e1eeafa3fa618d5fc619c83b2ee9b581",
+    PORT: 5432,
+    DIALECT: "postgres",
+    DIALECT_OPTION: {
+      ssl: { rejectUnauthorized: false },
+    },
+    QUERY: { raw: true },
+  },
+  PORT: {
+    HOST: 8080,
+  },
+  CLOUDINARY: {
+    CLOUD_NAME: "testappdinj123q",
+    API_KEY: "947624955641163",
+    API_SECRET: "9LNbFvteQzDqnx4JMhN1WE0209U",
+    SECURE: true,
+  },
+};
